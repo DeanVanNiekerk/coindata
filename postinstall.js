7649020b703ea2b6307edfe02a386a1a8afa911d
Dean van Niekerk <deanvniekerk@gmail.com>
2018-05-13 16:33:59 +0200

    update postinstall message

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,6 @@
 const emoji = require('node-emoji');
 
+console.log(``);
 console.log(`Thank you for installing coindata. Don't forget to ${emoji.get('star')}  us on GitHub!`);
+console.log(`https://github.com/DeanVanNiekerk/coindata`);
+console.log(``);
